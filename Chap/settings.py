bf8567009bed2e8c3792956a452150e2dfad5a11
Mohammad Hojjat Nikoobakht <m.h.nikoobakht@gmail.com>
2025-01-03 18:54:07 +0330

    feat: Configure PostgreSQL as the database for Django

--- Chap/settings.py
+++ Chap/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'Chap.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'chap888db',
+        'USER': 'chap888user',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
     }
 }
 
