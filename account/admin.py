a0f0fe86a9140768d4c64e9a4ed381101200f5a5
Mohammad Hojjat Nikoobakht <m.h.nikoobakht@gmail.com>
2025-01-05 13:05:54 +0330

    feat(account): Add user forms and admin configuration
    - Created UserCreationForm and UserChangeFormNew for custom user management - Implemented phone number validation in UserCreationForm - Updated admin.py to include custom UserAdmin - Configured search fields, list display, and fieldsets in UserAdmin - Unregistered Group model from admin

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+from .forms import UserCreationForm, UserChangeFormNew
+from django.contrib.auth.models import Group
 
-# Register your models here.
+admin.site.unregister(Group)
+
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    ordering = ['phone']
+
+    add_form = UserCreationForm
+    form = UserChangeFormNew
+    model = User
+
+    search_fields = [
+        'phone',
+        'first_name',
+        'last_name',
+        'email'
+    ]
+
+    list_display = [
+        'phone',
+        'first_name',
+        'last_name',
+        'email',
+        'is_active',
+        'is_staff',
+    ]
+
+    fieldsets = (
+        ('اصلی', {'fields': ('phone', 'password')}),
+        ('اطلاعات شخصی', {'fields': ('first_name', 'last_name', 'email')}),
+        ('دسترسی ها', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        ('تاریخ ها', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        ('اصلی', {'fields': ('phone', 'password1', 'password2')}),
+        ('اطلاعات شخصی', {'fields': ('first_name', 'last_name', 'email')}),
+        ('دسترسی ها', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('تاریخ ها', {'fields': ('last_login', 'date_joined')}),
+    )
