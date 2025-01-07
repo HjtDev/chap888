9e1154d118483ed060cef90301018501c91234e0
Mohammad Hojjat Nikoobakht <m.h.nikoobakht@gmail.com>
2025-01-07 11:14:00 +0330

    Initial Cart App

--- Chap/settings.py
+++ Chap/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main.apps.MainConfig',
-    'account.apps.AccountConfig'
+    'account.apps.AccountConfig',
+    'cart.apps.CartConfig'
 ]
 
 MIDDLEWARE = [
