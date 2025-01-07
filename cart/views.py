5828a41fcc5a0e91acd6c44c692dbf51ab100eac
Mohammad Hojjat Nikoobakht <m.h.nikoobakht@gmail.com>
2025-01-07 17:13:40 +0330

    test(cart): Add test views for cart functionality

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def test_view(request):
+    cart = Cart(request)
+    cart.add(Documents.objects.first().id, 1, 'A4', 'W&B', 'ONE_SIDE', 'NO_BINDING')
+    cart.add(Documents.objects.last().id, 2, 'A3', 'C50', 'BOTH_SIDES', 'COVERED_PUNCHED')
+    items = [item for item in cart]
+
+    return JsonResponse({'ok': True})
