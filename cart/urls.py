e9a9aa20a4c7222f8ce370140b2eb7293ea10e89
Mohammad Hojjat Nikoobakht <m.h.nikoobakht@gmail.com>
2025-01-08 14:59:51 +0330

    feat(cart): Add save button functionality and prevent rapid save calls in cart.html
     - Implemented save button functionality in cart.html to allow users to save their cart items - Added a 3-second timeout after each save to prevent rapid consecutive save calls

--- cart/urls.py
+++ cart/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.cart_view, name='cart'),
     path('test/', views.test_view, name='test'),
     path('upload/', views.upload_view, name='upload'),
+    path('save/', views.save_view, name='save'),
     path('delete/', views.delete_view, name='delete'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -2,10 +2,12 @@ from django.http import JsonResponse
 from django.shortcuts import render
 from .cart import Cart
 from .models import Documents
+from json import loads
 
 
 def cart_view(request):
     return render(request, 'cart.html', {'cart': Cart(request)})
+
 
 def test_view(request):
     cart = Cart(request)
@@ -14,6 +16,7 @@ def test_view(request):
     items = [item for item in cart]
 
     return JsonResponse({'ok': True})
+
 
 def delete_view(request):
     if request.method == 'POST':
@@ -36,6 +39,19 @@ def upload_view(request):
             doc = Documents.objects.create(pdf=pdf_file)
             cart = Cart(request)
             cart.add(doc.id, 1, 'A4', 'W&B', 'BOTH_SIDES', 'NO_BINDING')
-            return JsonResponse({'ok': True, 'thumbnail': doc.thumbnail.url, 'filename': str(doc), 'id': doc.id, 'pages': doc.get_page_count()})
+            return JsonResponse({'ok': True, 'thumbnail': doc.thumbnail.url, 'filename': str(doc), 'id': doc.id,
+                                 'pages': doc.get_page_count()})
         return JsonResponse({'ok': False, 'error': 'فایل آپلود نشد لطفا اینترنت خود را برسی کرده و مجددا تلاش کنید'})
     return JsonResponse({'ok': False, 'error': 'مشکلی در آپلود فایل به وجود امد لطفا مجددا تلاش کنید.'})
+
+
+def save_view(request):
+    print('called')
+    if request.method == 'POST':
+        data = loads(request.body)
+        cart = Cart(request)
+        for doc_id, options in data.items():
+            result = cart.add(doc_id, **options)
+            if isinstance(result, str):
+                return JsonResponse({'ok': False, 'error': result})
+        return JsonResponse({'ok': True})
