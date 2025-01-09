c2a8f5f2ec341d34f3462b5f39b41faf7f8c69d7
Mohammad Hojjat Nikoobakht <m.h.nikoobakht@gmail.com>
2025-01-09 12:15:23 +0330

    fix(cart): Correct invalid URL and add title to cart.html

--- cart/urls.py
+++ cart/urls.py
@@ -5,7 +5,6 @@ app_name = 'cart'
 
 urlpatterns = [
     path('', views.cart_view, name='cart'),
-    path('test/', views.test_view, name='test'),
     path('upload/', views.upload_view, name='upload'),
     path('save/', views.save_view, name='save'),
     path('delete/', views.delete_view, name='delete'),
