ed2af07295bbe79fe15adedc3db3c6381c753b35
Mohammad Hojjat Nikoobakht <m.h.nikoobakht@gmail.com>
2025-01-18 16:13:25 +0330

    fix(settings): Fix all unwanted modifications

--- Chap/wsgi.py
+++ Chap/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Chap.prices')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Chap.settings')
 
 application = get_wsgi_application()
